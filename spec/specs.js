3a3a5848c8822db721d8f0940d903c3ed316b2be
Corrie Smith & Michael Summers <student@epicodus.com>
2015-09-08 11:24:48 -0700

    determines whether integer is greater than zero

--- spec/specs.js
+++ spec/specs.js
@@ -2,4 +2,8 @@ describe('factorialFactory', function () {
   it("multiplies all positive integers that make up the given number", function () {
     expect(factorialFactory(5)).to.equal(120);
   });
+
+  it("determines whether or not integer is greater than zero", function () {
+    expect(factorialFactory(0)).to.equal(1);
+  });
 });
